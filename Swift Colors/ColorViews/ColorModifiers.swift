678b9ea79a7feabff7d6c6a41a31c98faff12691
Stewart Lynch <slynch@createchsol.com>
2025-01-08 17:27:44 -0800

    Completed Project

--- Swift Colors/ColorViews/ColorModifiers.swift
+++ Swift Colors/ColorViews/ColorModifiers.swift
@@ -19,9 +19,10 @@ struct ColorModifiers: View {
     var body: some View {
         ScrollView {
             SplitColorScheme {
-                Section("Modifiers") {
-
-                }
+                Color.purple.opacity(0.5)
+                Color.purple.brightness(0.5)
+                Color.purple.grayscale(0.5)
+                Color.red.mix(with: .green, by: 0.5)
             }
         }
     }
